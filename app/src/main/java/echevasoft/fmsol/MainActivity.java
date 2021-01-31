66a38ef4a0a11df09a0535540d4a72b9fb8d0658
echvafacu555 <echevarriafacundo555@gmail.com>
2021-01-31 16:19:26 -0300

    1ero

--- app/src/main/java/echevasoft/fmsol/MainActivity.java
+++ app/src/main/java/echevasoft/fmsol/MainActivity.java
@@ -116,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -229,7 +230,7 @@ public class MainActivity extends AppCompatActivity {
                 finish();
 
                 System.exit(0);
-
+//aa
             }
         });
         builder.setNegativeButton("Minimizar", new DialogInterface.OnClickListener() {
